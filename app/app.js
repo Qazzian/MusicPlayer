783263f5df9e2dcbb2a2e088d1b8b477cfa007b4
Qazzian <Qazzian@gmail.com>
2015-05-26 13:08:49 +0100

    Get songs and filter/order them

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var musicApp = angular.module('myApp', [
+  //'myApp.search'
+]);
+
+musicApp.controller('musicSearchCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('/private/songs.json').success(function(data){
+    $scope.availableSongs = data;
+  });
+
+  $scope.searchQuery = '';
+  $scope.searchOrderBy = 'artist';
 }]);
